7d90a7168d7a3da71c17eedd115c23066a70cd20
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-02-09 14:36:58 +0200

    A new updated repository; includes a custom web server and MVC

---
+++ StartUp.cs
@@ -0,0 +1,19 @@
+﻿using BasicWebProject.Services;
+using BasicWebServer.Routing;
+
+namespace BasicWebServer.Demo
+{
+    public class Startup
+    {
+        public static async Task Main()
+        {
+            var server = new HttpServer(routes => routes
+               .MapControllers()
+               .MapStaticFiles());
+
+            server.ServiceCollection.Add<UserService>();
+
+            await server.Start();
+        }
+    }
+}
